2aa31cb55ae8f2fd2a54b29f285e95ed12c6839e
Gustaf Dalemar <gustaf.dalemar@gmail.com>
2015-10-29 18:15:09 +0100

    Added Netlight image

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var koa = require('koa');
 var app = koa();
 
 app.use(function *(){
-  this.body = 'Hello Netlight!!';
+  this.body = '<img src="https://www.netlight.com/wp-content/themes/new_netlight_main/static/images/logo.png"/> Hello Netlight!!';
 });
 
 app.listen(process.env.PORT || 3000);
